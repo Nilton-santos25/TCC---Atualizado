ea94e36e1240071257a1ebef8a5e5cb782e6b257
Nilton Santos <niltonsantosmf2@gmail.com>
2021-11-15 10:46:51 -0300

    tcc atualizado

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View } from 'react-native';
+import { Profile } from '../../components/Profile';
+
+import { ButtonAdd } from '../../components/ButtonAdd';
+import { CardCategory } from '../../components/CardCategory'
+import { styles } from './styles';
+
+export function Home() {
+    return(
+        <View>
+            <View style={styles.header}>
+                <Profile />
+                {/* <ButtonAdd /> */}
+            </View>
+
+            <View>
+                <CardCategory />
+            </View>
+
+        </View>
+
+        
+    )
+}
